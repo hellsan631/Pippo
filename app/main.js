7ab9ac12b78c6d632b3a3ba1886a54cd89def19d
Mathew Kleppin <matkle414@gmail.com>
2015-11-19 02:25:35 -0600

    counting

--- app/main.js
+++ app/main.js
@@ -1,17 +1,81 @@
-var bPromise = require('bluebird');
-
-var advertizers = [
+var advertisers = [
   'EMC Corporation',
   'Black & Decker Corp',
-  'Berkshire Hathaway',
-  'Target Corporation',
-  'Pepco Holdings, Inc.',
-  'CarMax',
-  'Ecolab Inc.'
+  'Berkshire Hathaway'
 ];
 
-function Resol(){
+var clients = [
+  'Henderson Alvarez',
+  'Jon Lester',
+  'Troy Tulowitzki'
+];
+
+exports = module.exports = Resol;
+
+function Resol() {
 
 }
 
-module.exports = Resol;
+Resol.prototype.run = function() {
+  var _this = this;
+
+
+};
+
+Resol.prototype.combine = function(adver, client) {
+  var _this = this;
+
+  var total = 0;
+
+  if (adver.length % 2 === 0) {
+    total = _this.countVowels(client) * 1.5;
+  } else {
+    total = _this.countConcents(client);
+  }
+
+  var commonFactors = _this.countDuplicates(
+    _this.findFactors(adver.length),
+    _this.findFactors(client.length)
+  );
+
+  if (commonFactors > 0) {
+    total = total * 1.5;
+  }
+
+  return total;
+
+};
+
+Resol.prototype.countVowels = function(name) {
+  return name.match(/[aeiou]/gi).length;
+};
+
+Resol.prototype.countConcents = function(name) {
+  return name.length - this.countVowels(name);
+};
+
+Resol.prototype.countDuplicates = function(one, two) {
+  var count = 0;
+
+  for (var i = 0; i < one.length; i++) {
+    for (var k = 0; k < two.length; k++) {
+      if (one[i] === two[k]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
+
+Resol.prototype.findFactors = function(number) {
+  var factors = [];
+
+  for (var i = 2; i < number; i++) {
+    if (number % i === 0){
+      factors.push(i);
+    }
+  }
+
+  return factors;
+};
---
+++ tests/resol.test.js
@@ -0,0 +1,5 @@
+var Resol = require('../app/main.js');
+
+var ctr = new Resol();
+
+ctr.run();
