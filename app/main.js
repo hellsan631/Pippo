5960bb8e7d92d26959e897da0ab15b36bd5da929
Mathew Kleppin <matkle414@gmail.com>
2015-11-19 00:51:23 -0600

    init commit

---
+++ app/main.js
@@ -0,0 +1,17 @@
+var bPromise = require('bluebird');
+
+var advertizers = [
+  'EMC Corporation',
+  'Black & Decker Corp',
+  'Berkshire Hathaway',
+  'Target Corporation',
+  'Pepco Holdings, Inc.',
+  'CarMax',
+  'Ecolab Inc.'
+];
+
+function Resol(){
+
+}
+
+module.exports = Resol;
